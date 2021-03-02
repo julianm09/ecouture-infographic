6a79e346759077a437a1527a8c84a687adcd5129
Robert Smith <razzira11@gmail.com>
2021-03-02 00:06:34 -0800

    created button

---
+++ comps/BlueButton/index.js
@@ -0,0 +1,19 @@
+function MyBlueButtonUI(){
+    return `
+    <div style="
+    color: white;
+    background-color: #3884FF;
+    height:50px;
+    max-width:300px;
+    border-radius: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: Arial;
+    font-size: x-large;
+    ">
+    Continue
+    </div>`
+}
+
+export const BlueButtonUI = MyBlueButtonUI();
