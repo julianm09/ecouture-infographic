4985cb6c739a4bb7eb80c55abab51aa8355aa08e
sgill433@my.bcit.ca <sgill433@my.bcit.ca>
2021-03-06 20:10:30 -0800

    added water droplet animation

--- pages/animal.js
+++ pages/animal.js
@@ -17,8 +17,8 @@ const gallery = [
     text: "The ground on sheep farms is prone to overgrazing. This leads to a <strong>lack of nutrients and strength in the soil </strong> over time, degrading and weakening it.",
   },
   {
-    img: "animation",
-    text: "text",
+    img: Drop(),
+    text: "<strong>Recycled Wool</strong> uses less water compared to virgin wool by a factor of 9.",
   },
   {
     img: "animation",
