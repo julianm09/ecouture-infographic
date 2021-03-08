c8f28fcdf045067ad1aa3df98e0d8d3e3dd0e164
Julian <juliantmayes@gmail.com>
2021-03-08 13:47:17 -0800

    consitent sizing on all pages

--- comps/BlueButton/index.js
+++ comps/BlueButton/index.js
@@ -1,17 +1,30 @@
-function MyBlueButtonUI(text="Continue"){
+function BlueButtonUI(text="Continue", link="../index.html"){
+
+
+    handleClick = () => {
+       window.location.href = link
+
+    }
+
+
     return `
-    <div style="
+    <div 
+    onclick="handleClick()"
+    style="
+    cursor:pointer;
     color: white;
     background-color: #3884FF;
     height:50px;
-    max-width:300px;
     border-radius: 12px;
     box-shadow: 4px 4px 10px grey;
     display: flex;
     justify-content: center;
     align-items: center;
-    font-family: Arial;
-    font-size: x-large;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 20px;
+    font-weight 700;
+    width:80vw;
+
     ">
     ${text}
     </div>`
--- pages/finish.js
+++ pages/finish.js
@@ -1,1 +1,3 @@
 document.getElementById('row1').innerHTML += Logo('../comps/Resources/earth-small.png')
+
+let homeButton = document.getElementById('row4').innerHTML += BlueButtonUI('Back Home', '../index.html')
