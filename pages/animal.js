e68ef5e0696390dca5757ec5a5cf0f2079728ac6
Julian <juliantmayes@gmail.com>
2021-03-08 14:41:48 -0800

    facebook share

--- pages/animal.js
+++ pages/animal.js
@@ -6,35 +6,51 @@ const question = document.querySelector(".question");
 const gallery = [
   {
     img: Bottle(),
-    text: "A pair of jeans made from 1kg of cotton fiber can take up to <strong>20,000 litres</strong> of water to produce.",
+    text:
+      "A pair of jeans made from 1kg of cotton fiber can take up to <strong>20,000 litres</strong> of water to produce.",
   },
   {
     img: Gas(),
-    text: "Sheep produce around 19,561,740 kg of methane gas annually, one of the most toxic gases for our planet. 1 kg of methane can heat up Earth nearly <strong> 80x more than CO2 </strong> in some cases.",
+    text:
+      "Sheep produce around 19,561,740 kg of methane gas annually, one of the most toxic gases for our planet. 1 kg of methane can heat up Earth nearly <strong> 80x more than CO2 </strong> in some cases.",
   },
   {
     img: Tree(),
-    text: "The ground on sheep farms is prone to overgrazing. This leads to a <strong>lack of nutrients and strength in the soil </strong> over time, degrading and weakening it.",
+    text:
+      "The ground on sheep farms is prone to overgrazing. This leads to a <strong>lack of nutrients and strength in the soil </strong> over time, degrading and weakening it.",
   },
   {
     img: Drop(),
-    text: "<strong>Recycled Wool</strong> uses less water compared to virgin wool by a factor of 9.",
+    text:
+      "<strong>Recycled Wool</strong> uses less water compared to virgin wool by a factor of 9.",
   },
   {
     img: SheepIcon(),
-    text: "Modern day sorting methods <strong>remove the need to dye</strong> recycled wools, reducing water and chemical usage drastically.",
+    text:
+      "Modern day sorting methods <strong>remove the need to dye</strong> recycled wools, reducing water and chemical usage drastically.",
   },
   {
     img: earthIcon(),
-    text: "Wool’s longer lifespan means that while it only makes up 1.3% of the global fibre supply, they are <strong>donated at a rate of 5%.</strong>",
+    text:
+      "Wool’s longer lifespan means that while it only makes up 1.3% of the global fibre supply, they are <strong>donated at a rate of 5%.</strong>",
   },
 ];
 
 // pass all of gallery through scroll function with three params //
 // ScrollContainer(index, image, text) //
 for (var i = 1; i < 7; i++) {
-  scrollContainer.innerHTML += ScrollContainer(i, gallery[i - 1].img, gallery[i - 1].text);
+  scrollContainer.innerHTML += ScrollContainer(
+    i,
+    gallery[i - 1].img,
+    gallery[i - 1].text
+  );
 }
 
-question.innerHTML += QuestionUI("Which material is more sustainable?", "Cotton", "Recycled Cotton", "../pages/wrong.html", "../index.html", 'animal')
-
+question.innerHTML += QuestionUI(
+  "Which material is more sustainable?",
+  "Cotton",
+  "Recycled Cotton",
+  "../pages/wrong.html",
+  "../index.html",
+  "animal"
+);
